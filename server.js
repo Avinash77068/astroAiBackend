decec45f08c6dcebcdb54fd2199fd9d59cb980e3
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2026-01-25 10:28:09 +0530

    code commited

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const userRoutesController = require("./src/routes/userRoute.js");
 dotenv.config();
 
 const app = express();
-
+const api ="api"
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Routes
-app.use("/homepage", homepageRoutes);
-app.use("/astrologer", astrologerRoutes);
-app.use("/user", userRoutesController);
+app.use("/api/homepage", homepageRoutes);
+app.use("/api/astrologer", astrologerRoutes);
+app.use("/api/user", userRoutesController);
 
 app.listen(3000, () => {
     console.log("Server started on port http://localhost:3000");
